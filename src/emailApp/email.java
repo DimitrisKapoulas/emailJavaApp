b1984f5a1c41f63afd2e9057d8bc0d4c46194cdd
DimitrisKapoulas <kapoulasd@gmail.com>
2019-08-28 16:43:56 +0300

    Setup the constructor, plus testing !!

--- src/emailApp/email.java
+++ src/emailApp/email.java
@@ -11,6 +11,12 @@ public class email {
 
     // Constructor to receive the first name and the last name
 
+    public email(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        System.out.println("Email Created: "+this.firstName+"" +this.lastName);
+    }
+
     // Ask for the department
 
     // Generate a random password
@@ -20,6 +26,5 @@ public class email {
     // Set the alternate email
 
     // Change the password
-    
 
 }
--- src/emailApp/emailApp.java
+++ src/emailApp/emailApp.java
@@ -3,5 +3,6 @@ package emailApp;
 public class emailApp {
     public static void main(String[] args){
 
+        email em1 = new email("John","Smith");
     }
 }
